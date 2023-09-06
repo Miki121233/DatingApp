dac833bf33053a789ac416014597bbce11c822c4
MPR21 <mikolaj.PLADER.intern@3ds.com>
2023-09-06 22:17:14 +0200

    Async pipe instead of loggedIn boolean

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
+import { Observable } from 'rxjs/internal/Observable';
+import { of } from 'rxjs/internal/observable/of';
 
 @Component({
   selector: 'app-nav',
@@ -8,26 +11,16 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit{
   model: any = {};
-  loggedIn = false;
 
-  constructor(private accountService: AccountService) {}
+  constructor(public accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.getCurrentUser();
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe({
-      next: user => this.loggedIn = !!user,
-      error: error => console.log(error)
-    })
   }
 
   login(){
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
     })
@@ -35,7 +28,6 @@ export class NavComponent implements OnInit{
 
   logout(){
     this.accountService.logout();
-    this.loggedIn = false;
   }
 
 }
