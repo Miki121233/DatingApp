93f6e322bfaeb7873e6341e4b21419aacd6c07e3
MPR21 <mikolaj.PLADER.intern@3ds.com>
2023-09-07 22:34:04 +0200

    Routing added

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { AccountService } from '../_services/account.service';
 import { User } from '../_models/user';
 import { Observable } from 'rxjs/internal/Observable';
 import { of } from 'rxjs/internal/observable/of';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,22 +13,21 @@ import { of } from 'rxjs/internal/observable/of';
 export class NavComponent implements OnInit{
   model: any = {};
 
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService, private router: Router) {}
 
   ngOnInit(): void {
   }
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
+      next: _ => this.router.navigateByUrl('/members'),
       error: error => console.log(error)
     })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
