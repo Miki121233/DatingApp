4be3f82a00e359d4e6bb5bd2e8352b687904651d
MPR21 <mikolaj.PLADER.intern@3ds.com>
2023-09-09 19:50:02 +0200

    AppUser extended, Seeding for app testing

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using API.Extensions;
 
 namespace API.Entities;
 public class AppUser
@@ -6,8 +7,23 @@ public class AppUser
     public int Id { get; set; }
     [Required]
     public string UserName { get; set; }
-    
     public byte[] PasswordHash { get; set; }
-
     public byte[] PasswordSalt { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string KnownAs { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public string Gender { get; set; }
+    public string Introduction { get; set; }
+    public string Lookingfor { get; set; }
+    public string Interests { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public List<Photo> Photos { get; set; } = new();
+
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    }
+    
 }
