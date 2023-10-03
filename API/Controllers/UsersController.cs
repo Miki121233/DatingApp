94f1b362c7443b97fd70013ee0fb3bad68d03387
MPR21 <mikolaj.PLADER.intern@3ds.com>
2023-10-03 21:50:01 +0200

    Paging improved, Filtering added

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -26,6 +26,14 @@ public class UsersController : BaseApiController
     [HttpGet]
     public async Task<ActionResult<PagedList<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
     {
+        var currentUser = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+        userParams.CurrentUsername = currentUser.UserName;
+
+        if (string.IsNullOrEmpty(userParams.Gender))  
+        {   //for making a filtering key
+            userParams.Gender = currentUser.Gender == "male" ? "female" : "male";
+        }
+
         var users = await _userRepository.GetMembersAsync(userParams);
     
         Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages));
