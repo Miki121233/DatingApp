c5fe9c52d6601ba2e832cd2557c0ce9ea9137dee
MPR21 <mikolaj.PLADER.intern@3ds.com>
2023-10-11 21:00:54 +0200

    Likes Pagination

--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -1,10 +1,11 @@
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 
 namespace API.Interfaces;
 public interface ILikesRepository
 {
     Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);    
     Task<AppUser> GetUserWithLikes(int userId);    
-    Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);    
+    Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams);    
 }
